b7e587ca331d7ec7d79c9236a6b5d393170f6b6d
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-02-26 09:45:41 +0800

    update autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var postcss = require('gulp-postcss');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer-core');
 
 gulp.task('sass', function() {
   return gulp.src('./source/sass/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(postcss([autoprefixer({
+      browsers: ['last 2 version']
+    })]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./source/css'));
 });
 
