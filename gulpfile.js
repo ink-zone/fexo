9bd13e860118b9b7b1dadc565a99c802e29bb331
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-03-08 17:44:53 +0800

    add reveasy

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,30 @@ var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
+var reveasy = require('gulp-rev-easy');
+var path = require('path');
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch('./source/sass/**/*.scss', ['sass']);
+});
 
 gulp.task('sass', function() {
   return gulp.src('./source/sass/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
     .pipe(postcss([autoprefixer()]))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./source/css'));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch('./source/sass/**/*.scss', ['sass']);
-});
+
+gulp.task('rev', function(argument) {
+  gulp.src('./layout/_partial/style.ejs')
+    .pipe(reveasy({
+      base: path.join(process.cwd(), 'source'),
+      fileTypes: ['css']
+    }))
+    .pipe(gulp.dest('./layout/_partial'))
+})
