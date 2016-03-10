ffad68e80022a27f563d7d184504c845d6815e2a
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-03-10 19:30:43 +0800

    improve backtop

--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,24 @@
 (function() {
   'use strict';
 
-  var body = document.body;
+  var $body = document.body;
+  var $toc = $('#toc');
+  var $backTop = $('#backTop');
 
-  // toc
-  $(document).on('scroll', function () {
-    body.scrollTop > 200 ? $('#toc').addClass('fixed'): $('#toc').removeClass('fixed')
+  // toc and backTop
+  $(document).on('scroll', function() {
+    var scrollTop = $body.scrollTop;
+    scrollTop > 200 ? $toc.addClass('fixed') : $toc.removeClass('fixed');
+
+    scrollTop > 10 ? $backTop.addClass('show') : $backTop.removeClass('show');
+  })
+
+
+  // back top
+  $backTop.on('click', function() {
+
+    console.log(11);
+    $('body').animate({scrollTop: 0}, 'slow');
   })
 
 }());
